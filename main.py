2ca2f9ba4bace5e42d17e803dbffae479d180cf7
Jaime Cabrera <cabrerajaime877@gmail.com>
2023-01-16 16:29:10 -0600

    Day 33: Fixing errors

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
     # Get the temperature/sky data
-    filtered_data = get_data(place, days)
-
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures,
-                         labels={"x": "Dates", "y": "Temperatures"})
-        st.plotly_chart(figure)
-    elif option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_condition]
-        # Create a sky condition image
-        st.image(image_paths, width=116)
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            temperatures_celsius = [temp/10 for temp in temperatures]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures_celsius,
+                             labels={"x": "Dates", "y": "Temperatures  °C"})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_condition]
+            # Create a sky condition image
+            st.image(image_paths, width=116)
+    except KeyError:
+        st.warning("That place was not found. please write another",
+                   icon="⚠️")
